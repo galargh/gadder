5d67580f2990117e26f4f302b17488efcb457aaa
galargh <piotr.galar@gmail.com>
2022-04-12 14:34:11 +0200

    Move Chrome Extension logic to content

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# GAdder
+# ![GAdder Logo](images/gadder64.png) GAdder
 
 Welcome to **GAdder** - *GitHub Projects (Beta) Helper*.
--- content.js
+++ content.js
@@ -1,91 +1,164 @@
-const runs = {};
 const projectDetails = JSON.parse(document.getElementById('memex-item-create-api-data').innerHTML)
-projectDetails.name = document.getElementById('memexTitleInput').value
+const repoSearcherInput = document.querySelector('input[data-test-id="repo-searcher-input"]');
+const repoSearcherList = document.querySelector('ul[data-test-id="repo-searcher-list"]');
+const newItemButton = document.querySelector('button[data-test-id="new-item-button"]');
 
-async function search(run) {
-  const allResults = [];
+function parseHTML(html) {
+  return new DOMParser().parseFromString(html, 'text/html');
+}
 
-  var next = 'https://github.com/search?p=1&q=' + encodeURIComponent(run.query) + '&type=Issues';
+function getGadderRepoSearcherList() {
+  let gadderRepoSearcherList = document.querySelector('ul[data-test-id="gadder-repo-searcher-list"]');
+  if (! gadderRepoSearcherList) {
+    gadderRepoSearcherList = parseHTML(`
+      <ul data-test-id="gadder-repo-searcher-list" class="${repoSearcherList.className}" hidden></ul>
+    `).body.firstChild;
+    repoSearcherList.parentElement.append(gadderRepoSearcherList);
+  }
+  return gadderRepoSearcherList;
+}
+
+async function searchIssues(url) {
+  const response = await fetch(url);
+  const html = await response.text();
+  const element = parseHTML(html);
+  const count = Array.from(element.querySelector('div.codesearch-results h3').textContent.matchAll('\\d'), m => m[0]).join('');
+  const counter = element.querySelector('span[data-search-type="Issues"]')
+  const issues = [];
+  element.querySelectorAll('div[id="issue_search_results"] div.issue-list-item').forEach(div => {
+    const a = div.querySelector('div.markdown-title a');
+    issues.push({
+      svg: div.querySelector('svg').outerHTML,
+      title: a.title,
+      href: a.href
+    });
+  });
+  const next = element.querySelector('[rel="next"]');
+  return {
+    count: count,
+    counter: counter,
+    issues: issues,
+    next: next && next.href
+  };
+}
+
+async function searchIssuesPaginated(query) {
+  const allIssues = [];
+
+  let next = 'https://github.com/search?p=1&q=' + encodeURIComponent(query) + '&type=Issues';
   while(next) {
-    const response = await fetch(next).catch(console.log);
-    const html = await response.text();
-    const element = new DOMParser().parseFromString(html, 'text/html');
-    const results = element.querySelector('[id="issue_search_results"]');
-    results.querySelectorAll('div.markdown-title a').forEach(a => {
-      allResults.push(a.href);
-    });
-    const nextLink = element.querySelector('[rel="next"]');
-    next = nextLink && nextLink.href;
-    run.items = {
-      count: Array.from(element.querySelector('div.codesearch-results h3').textContent.matchAll('\\d'), m => m[0]).join(''),
-      fetched: allResults.length
-    };
-    chrome.runtime.sendMessage({
-      from: 'content',
-      subject: 'updateRun',
-      payload: run
-    });
+    const issues = await searchIssues(next);
+    allIssues.push(...issues.issues);
+    next = issues.next;
+    console.log(`Found ${allIssues.length} out of ${issues.count} issues`);
   }
 
-  return allResults;
+  return allIssues;
 }
 
-async function add(issues) {
-  const allResponses = [];
 
-  for (let issueIndex = 0; issueIndex < issues.length; issueIndex++) {
-    const issue = issues[issueIndex];
-    const response = await fetch(projectDetails.url, {
-      method: 'POST',
-      headers: {
-        'accept': 'application/json',
-        'content-type': 'application/x-www-form-urlencoded',
-        'scoped-csrf-token': projectDetails.token,
-      },
-      body: [
-        encodeURIComponent('memexProjectItem[contentType]') + '=' + encodeURIComponent('DraftIssue'),
-        encodeURIComponent('memexProjectItem[content][title]') + '=' + encodeURIComponent(issue),
-        encodeURIComponent('memexProjectItem[memexProjectColumnValues][]')
-      ].join('&').replace(/%20/g, '+'),
-    }).catch(console.log);
-
-    allResponses.push(response);
-  }
-
-  return allResponses;
+function showGadderRepoSearcherList(query) {
+  searchIssues('https://github.com/search?p=1&q=' + encodeURIComponent(query) + '&type=Issues').then(response => {
+    const gadderRepoSearcherList = getGadderRepoSearcherList();
+    gadderRepoSearcherList.replaceChildren(...response.issues.map(issue => {
+      return parseHTML(`
+        <li style="cursor: pointer; display: block; font-size: 12px; padding: 8px;">
+          <div style="-webkit-box-align: center; align-items: center; overflow: hidden; flex: 1 1 0%; display: flex;">
+            <div style="flex-shrink: 0; margin-right: 8px; display: flex;">
+              ${issue.svg}
+            </div>
+            <span>${issue.title}</span>
+            <a target="_blank" href="${issue.href}" style="margin-left: 4px; color: rgb(87, 96, 106);">#${issue.href.split('/').reverse()[0]}</a>
+          </div>
+        </li>
+      `).body.firstChild;
+    }));
+    gadderRepoSearcherList.appendChild(parseHTML(`
+      <li style="cursor: pointer; display: block; font-size: 12px; padding: 8px;">
+        <div style="-webkit-box-align: center; align-items: center; overflow: hidden; flex: 1 1 0%; display: flex;">
+          <div title="${response.count}" style="flex-shrink: 0; margin-right: 8px; display: flex;">
+            ${response.counter.outerHTML}
+          </div>
+          <span id="query">${query}</span>
+        </div>
+      </li>`).body.firstChild);
+    gadderRepoSearcherList.hidden = false;
+    const repoSearcherInputRect = repoSearcherInput.getBoundingClientRect();
+    const gadderRepoSearcherListRect = gadderRepoSearcherList.getBoundingClientRect();
+    gadderRepoSearcherList.setAttribute('style', `left: ${repoSearcherInputRect.left}px; top: ${repoSearcherInputRect.top - gadderRepoSearcherListRect.height}px;`);
+  });
 }
 
-chrome.runtime.onMessage.addListener(function(msg, _sender, sendResponse) {
-  if (msg.from === 'popup' && msg.subject === 'addProjectItems') {
-    const run = {
-      id: crypto.randomUUID(),
-      status: 'In Progress',
-      name: projectDetails.name,
-      query: msg.payload.query,
-      startTime: Date.now()
-    };
-    chrome.runtime.sendMessage({
-      'from': 'content',
-      'subject': 'addRun',
-      'payload': run
-    });
-    runs[run.id] = search(run).then(add).then(responses => {
-      console.log(responses);
-      run.status = 'Completed';
-      chrome.runtime.sendMessage({
-        from: 'content',
-        subject: 'updateRun',
-        payload: run
-      });
-    });
-    sendResponse(run);
+function debounce(callback, delay) {
+  let timeout;
+  return () =>{
+    clearTimeout(timeout);
+    timeout = setTimeout(callback, delay);
   }
-  if (msg.from === 'background' && msg.subject === 'deleteRun') {
-    const run = msg.payload;
-  }
-});
+}
 
-chrome.runtime.sendMessage({
-  from: 'content',
-  subject: 'enablePopup',
+repoSearcherInput.addEventListener('keyup', debounce(() => {
+  const query = repoSearcherInput.value;
+  if (query.startsWith('q=') && query.length > 2) {
+    showGadderRepoSearcherList(query.substr(2));
+  } else {
+    getGadderRepoSearcherList().hidden = true;
+  }
+}, 1000));
+
+async function addItem(url) {
+  console.log(`Adding item from ${url}`);
+  const response = await fetch(projectDetails.url, {
+    method: 'POST',
+    headers: {
+      'accept': 'application/json',
+      'content-type': 'application/x-www-form-urlencoded',
+      'scoped-csrf-token': projectDetails.token,
+    },
+    body: [
+      encodeURIComponent('memexProjectItem[contentType]') + '=' + encodeURIComponent('DraftIssue'),
+      encodeURIComponent('memexProjectItem[content][title]') + '=' + encodeURIComponent(url),
+      encodeURIComponent('memexProjectItem[memexProjectColumnValues][]')
+    ].join('&').replace(/%20/g, '+'),
+  });
+  console.log(`Response: ${response.status}`);
+  return response;
+}
+
+repoSearcherList.parentElement.addEventListener('click', event => {
+  if (event.target.name !== 'a') {
+    const gadderRepoSearcherList = getGadderRepoSearcherList();
+    const gadderRepoSearcherListItem = Array.from(gadderRepoSearcherList.querySelectorAll('li')).find(li => li.contains(event.target));
+    if (gadderRepoSearcherListItem) {
+      event.preventDefault();
+      gadderRepoSearcherList.hidden = true;
+      repoSearcherInput.disabled = true;
+      newItemButton.disabled = true;
+      const a = gadderRepoSearcherListItem.querySelector('a');
+      if (a) {
+        addItem(a.href).then(response => {
+          newItemButton.disabled = false;
+          repoSearcherInput.disabled = false;
+          gadderRepoSearcherList.hidden = false;
+        });
+      } else {
+        searchIssuesPaginated(gadderRepoSearcherListItem.querySelector('span[id="query"]').textContent).then(async issues => {
+          const allResponses = [];
+
+          for (let issueIndex = 0; issueIndex < issues.length; issueIndex++) {
+            const issue = issues[issueIndex];
+            const response = await addItem(issue.href);
+            allResponses.push(response);
+            console.log(`Added ${allResponses.length} out of ${issues.length} issues`);
+          }
+
+          return allResponses;
+        }).then(responses => {
+          newItemButton.disabled = false;
+          repoSearcherInput.disabled = false;
+          gadderRepoSearcherList.hidden = false;
+        });
+      }
+    }
+  }
 });
